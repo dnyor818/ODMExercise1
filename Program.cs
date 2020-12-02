c5a387e4c1f0a02b2f4b26e00c80f23377051b04
Your Name <you@example.com>
2020-12-02 13:32:27 -0600

    Trying to init git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ODMExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
